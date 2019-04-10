cbd4a5438e8df1d6793d50f7e3d5bbaee8250328
chawen <@>
2019-04-10 15:38:11 -0300

    dev

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-git push --set-upstream https://github.com/wencha/pyflaskapp.git master
 git add .
 git commit -m'dev'
-git push https://github.com/wencha/pyflaskapp.git
+git push --set-upstream https://github.com/wencha/pyflaskapp.git master
