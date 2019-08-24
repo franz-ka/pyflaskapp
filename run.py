a11ed1b2c888d5116a69e8d03491f994a78386bf
Wencha <wencha_@hotmail.com>
2019-08-24 10:36:42 -0300

    codigo más prolijo

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask1.alarmas import check_alarmas
+
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(check_alarmas, 'interval', minutes=60)
 sched.start()
+
 
 from flask1 import create_app
 app = create_app()
