6ef510e34ebe09b56a4b7df7bb9d4b928be00efd
wencha <wencha_@hotmail.com>
2019-09-25 20:29:15 -0300

    nuevo script utilidad de modificar base de datos

---
+++ utils/db/dbconfig.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+app_path = (Path(__file__).parent.parent.parent).absolute()
+print('Usando path =', str(app_path))
+sys.path.append(str(app_path / 'flask1'))
+
+from models import Base, \
+    Usuario, \
+    Pika, Insumo, PikaInsumo, \
+    PrestockPika, StockPika, StockInsumo, MovStockPika, MovStockInsumo, \
+    VentaTipo, Venta, VentaPika, \
+    Maquina, Gcode, Falla, Alarma
+
+def init_db_engine():
+    from sqlalchemy import create_engine
+    __dbconnstr = 'sqlite:///' + str(app_path / 'instance' / 'flask1.db')
+    print('Usando conn str =', __dbconnstr)
+    return create_engine(__dbconnstr, echo=True)
+
+def get_db_session(create_new=False):
+    from sqlalchemy.orm import sessionmaker, scoped_session
+    engine = init_db_engine()
+    #Base.metadata.drop_all(engine)
+    if create_new:
+        Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    return DBSession()
