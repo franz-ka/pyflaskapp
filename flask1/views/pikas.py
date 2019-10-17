fb60598f8e77783f42a60712cee6e02d8e7e9b0b
wencha <wencha_@hotmail.com>
2019-10-17 17:40:49 -0300

    factor de impresion renombrado, y agregado venta diaria

--- flask1/views/pikas.py
+++ flask1/views/pikas.py
@@ -322,18 +322,43 @@ def menu_modificarcolorpikas():
 
         return ''
 
-@bp_pikas.route("/factoresdeproductividad", methods = ['GET', 'POST'])
+@bp_pikas.route("/factoresdeimpresion", methods = ['GET', 'POST'])
 @login_required
-def menu_factoresdeproductividad():
+def menu_factoresdeimpresion():
     if request.method == "GET":
         db = get_db()
         #pikas = db.query(Pika).order_by(Pika.nombre).all()
         #factores = db.query(FactorProductividad).all()
         pika_factores = db.query(Pika, FactorProductividad).outerjoin(FactorProductividad).all()
 
+        from sqlalchemy import func
+        dias_factor = 60.0
+        dtnow = datetime.datetime.now()
+        dtventas = dtnow - datetime.timedelta(days=dias_factor)
+        #print(dias_factor, dtventas)
+        
+        #ventapikas = db.query(VentaPika).join(Venta).filter(Venta.fecha != None)
+        ventapikas = db.query(
+            VentaPika.pika_id,
+            func.sum(VentaPika.cantidad/dias_factor).label('total')
+        ).join(Venta
+        ).filter(Venta.fecha != None
+        ).filter(Venta.fecha >= dtventas
+        ).group_by(VentaPika.pika_id
+        ).all()
+        #print(ventapikas)
+        
+        ventas_promedios = {}
+        for pika_id, venta_promedio in ventapikas:
+            ventas_promedios[pika_id] = "{0:.2f}".format(venta_promedio)
+        #print(ventas_promedios)
+
         r = make_response(render_template(
-            'menu/pikas/factoresdeproductividad.html',
-            pika_factores=pika_factores
+            'menu/pikas/factoresdeimpresion.html',
+            pika_factores=pika_factores,
+            dias_factor=dias_factor,
+            dtventas=dtventas,
+            ventas_promedios=ventas_promedios
         ))
         return r
     else: #request.method == "POST":
@@ -344,7 +369,6 @@ def menu_factoresdeproductividad():
 
         db = get_db()
         
-
         pika = db.query(Pika).get(request.form['pika_id'])
         factor_cant = float(request.form['factor_nuevo'])
         dtnow = datetime.datetime.now()
