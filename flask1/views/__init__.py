6706196305d0da3504039bcbd24f11433b97d0d8
wencha <wencha_@hotmail.com>
2019-10-17 17:49:07 -0300

    nueva categoría de menú: prioridades

--- flask1/views/__init__.py
+++ flask1/views/__init__.py
@@ -3,10 +3,12 @@ from .insumos import bp_insumos
 from .ventas import bp_ventas
 from .fallas import bp_fallas
 from .alarmas import bp_alarmas
+from .prioridades import bp_prioridades
 menu_bps = [
     bp_pikas,
     bp_insumos,
     bp_ventas,
     bp_fallas,
     bp_alarmas,
+    bp_prioridades,
 ]
