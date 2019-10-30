cebbb5e6e7ff517cb395fa890caad6b84a13331d
wencha <wencha_@hotmail.com>
2019-10-30 20:33:27 -0300

    update migración tipo insumos, ahora updetea automáticamente

--- utils/db/1.3/2migrar-insumos-tipos.py
+++ utils/db/1.3/2migrar-insumos-tipos.py
@@ -5,7 +5,7 @@ print('Usando path =', str(app_path))
 sys.path.append(str(app_path)) 
 
 
-from dbconfig import init_db_engine
+from dbconfig import init_db_engine, get_db_session, Insumo, InsumoTipo
     
 engine = init_db_engine()
 
@@ -32,4 +32,27 @@ DROP TABLE IF EXISTS _insumo_old;
 PRAGMA foreign_keys=on;
 '''.split(';') if len(l.strip())]
 
+#print(list(engine.execute('select * from insumo')))
+
+db = get_db_session()
+
+tipo_prestock = db.query(InsumoTipo).filter(InsumoTipo.nombre=='Prestock').one()
+tipo_armado = db.query(InsumoTipo).filter(InsumoTipo.nombre=='Armado').one()
+tipo_venta = db.query(InsumoTipo).filter(InsumoTipo.nombre=='Venta').one()
+tipo_consumible = db.query(InsumoTipo).filter(InsumoTipo.nombre=='Consumible').one()
+
+insus = db.query(Insumo).all()
+for insu in insus:
+    if insu.nombre.startswith(('X -','Y -','Z -','PLA ')):
+        insu.insumotipo = tipo_consumible
+    elif insu.nombre.startswith(('Packaging', 'Sticker')):
+        insu.insumotipo = tipo_venta
+    elif insu.nombre.startswith(('Imán', 'Argollas', 'Triangulito')):
+        insu.insumotipo = tipo_armado
+    elif insu.nombre.startswith('Oring'):
+        insu.insumotipo = tipo_prestock
+    
+db.commit()
+
 print(list(engine.execute('select * from insumo')))
+
