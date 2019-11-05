1c988ef0327098e5e9d870db59818f35a24133e0
wencha <wencha_@hotmail.com>
2019-11-05 14:12:21 -0300

    calcular pedidos en alarmas de insumos

--- flask1/alarmas.py
+++ flask1/alarmas.py
@@ -1,5 +1,6 @@
+from sqlalchemy import func
 from flask import g
-from flask1.models import Alarma, Insumo, StockInsumo
+from flask1.models import Alarma, Insumo, StockInsumo, VentaPika, Venta, PikaInsumo
 from datetime import datetime
 import smtplib
 
@@ -27,10 +28,26 @@ def check_alarma(insu):
     db = _get_db_app_or_out()
     
     alarma = db.query(Alarma).filter(Alarma.insumo == insu).first()
+    
     if alarma:
         stock = db.query(StockInsumo).filter(StockInsumo.insumo == insu).first()
-        mando_mail = check_insumo_stock(alarma, stock)
+        
+        q = db.query(func.sum(VentaPika.cantidad*PikaInsumo.cantidad).label('cant_insu_pedidos')) \
+            .join(Venta) \
+            .filter(Venta.fecha_pedido != None, Venta.fecha == None) \
+            .join(PikaInsumo, VentaPika.pika_id == PikaInsumo.pika_id) \
+            .filter(PikaInsumo.insumo_id == insu.id) \
+            .group_by(PikaInsumo.insumo_id) \
+            .all()
+            
+        if len(q):
+            pedidos_cant = q[0].cant_insu_pedidos
+        else:
+            pedidos_cant = 0
+        
+        mando_mail = check_insumo_stock(alarma, stock, pedidos_cant)  
         if mando_mail:
+            print('check_alarma mail', insu)
             alarma.fecha_avisado = datetime.now()
             db.commit()
         
@@ -39,30 +56,45 @@ def check_alarmas():
     db = _get_db_app_or_out()
 
     alarmas_stocks = db.query(Alarma, StockInsumo).filter(StockInsumo.insumo_id == Alarma.insumo_id).all()
+    
     for alarma, stock in alarmas_stocks:
-        mando_mail = check_insumo_stock(alarma, stock)
+        q = db.query(func.sum(VentaPika.cantidad*PikaInsumo.cantidad).label('cant_insu_pedidos')) \
+            .join(Venta) \
+            .filter(Venta.fecha_pedido != None, Venta.fecha == None) \
+            .join(PikaInsumo, VentaPika.pika_id == PikaInsumo.pika_id) \
+            .filter(PikaInsumo.insumo_id == stock.insumo_id) \
+            .group_by(PikaInsumo.insumo_id) \
+            .all()
+            
+        if len(q):
+            pedidos_cant = q[0].cant_insu_pedidos
+        else:
+            pedidos_cant = 0
+            
+        mando_mail = check_insumo_stock(alarma, stock, pedidos_cant)        
         if mando_mail:
+            print('check_alarmas mail', stock.insumo)
             alarma.fecha_avisado = datetime.now()
             db.commit()
          
 
-def check_insumo_stock(alarma, stock):
-    if stock.cantidad <= alarma.cantidad and (not alarma.fecha_avisado or days_between(alarma.fecha_avisado, datetime.now()) >= _alarma_dias_intervalo):
-        mandar_alarma(alarma.insumo.nombre, stock.cantidad, alarma.cantidad)
+def check_insumo_stock(alarma, stock, pedidos_cant):
+    if stock.cantidad - pedidos_cant <= alarma.cantidad and (not alarma.fecha_avisado or days_between(alarma.fecha_avisado, datetime.now()) >= _alarma_dias_intervalo):
+        mandar_alarma(alarma.insumo.nombre, stock.cantidad, pedidos_cant, alarma.cantidad)
         return True
     return False
 
 
-def mandar_alarma(nombre_insu, stock_insu, alarma_cant):
+def mandar_alarma(nombre_insu, stock_insu, pedidos_cant, alarma_cant):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.ehlo()
     s.starttls()
     s.ehlo()
     s.login("stockcogonauts@gmail.com", "Markdijono1375$")
     message = (
-            'Subject: Alarma stock bajo ({1}) de {0}\n'+
-            'Hay {1} de stock para el insumo {0}.\n\n'+
-            '(alarma configurada para stock igual o menor a {2})'
-        ).format(nombre_insu, stock_insu, alarma_cant)
+            'Subject: Alarma stock bajo ({3}) de {0}\n'+
+            'Hay {3} de stock para el insumo {0} ({1} stock real, {2} stock pedido).\n\n'+
+            '(alarma configurada para stock igual o menor a {4})'
+        ).format(nombre_insu, stock_insu, pedidos_cant, stock_insu - pedidos_cant, alarma_cant)
     s.sendmail("stockcogonauts@gmail.com", "cogonauts@gmail.com", message.encode('utf8'))
     s.quit()
