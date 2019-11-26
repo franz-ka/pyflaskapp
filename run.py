873b003e4f5ef718973369348db7a6e45bda7b1e
wencha <wencha_@hotmail.com>
2019-11-26 16:15:43 -0300

    Mandar mails solo si no está en debug

--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask1.alarmas import check_alarmas
 from flask1.backup_db import check_backup_db
 
-schedAlarmas = BackgroundScheduler(daemon=True)
-schedAlarmas.add_job(check_alarmas, 'interval', minutes=60)
-schedAlarmas.start()
-
-schedBackupDB = BackgroundScheduler(daemon=True)
-schedBackupDB.add_job(check_backup_db, 'interval', hours=2)
-schedBackupDB.start()
-
-
 from flask1 import create_app
 app = create_app()
+
+if not app.config['DEBUG_FLASK']:
+    schedAlarmas = BackgroundScheduler(daemon=True)
+    schedAlarmas.add_job(check_alarmas, 'interval', minutes=60)
+    schedAlarmas.start()
+    
+    schedBackupDB = BackgroundScheduler(daemon=True)
+    schedBackupDB.add_job(check_backup_db, 'interval', hours=2)
+    schedBackupDB.start()
+
 app.run(debug=app.config['DEBUG_FLASK'], host=app.config['APP_HOST'], port=app.config['APP_PORT'])
