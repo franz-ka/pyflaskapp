fcc655a9481732773c025b0be7a7e9030e08c824
anon <>
2021-05-05 14:04:23 -0300

    mover función usada checkboxs UpdateQueryString a js global de forms

--- flask1/static/js/common_forms.js
+++ flask1/static/js/common_forms.js
@@ -92,3 +92,39 @@ function sel2(clas, placeh, data){
         data: data
     });
 }
+
+// How can I add or update a query string parameter?
+// https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
+function UpdateQueryString(key, value, url) {
+    if (!url) url = window.location.href;
+    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+        hash;
+
+    if (re.test(url)) {
+        if (typeof value !== 'undefined' && value !== null) {
+            return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        }
+        else {
+            hash = url.split('#');
+            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+                url += '#' + hash[1];
+            }
+            return url;
+        }
+    }
+    else {
+        if (typeof value !== 'undefined' && value !== null) {
+            var separator = url.indexOf('?') !== -1 ? '&' : '?';
+            hash = url.split('#');
+            url = hash[0] + separator + key + '=' + value;
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+                url += '#' + hash[1];
+            }
+            return url;
+        }
+        else {
+            return url;
+        }
+    }
+}
